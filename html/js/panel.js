e2d3c5a10d3b804b6d8205ae9976871101a4d58d
Mazensh88 <63317015+Mazensh88@users.noreply.github.com>
2021-10-19 22:40:19 -0400

    JS connected
    fixed some styles

---
+++ html/js/panel.js
@@ -0,0 +1,20 @@
+let fullName = document.getElementById("fullName").innerHTML;
+console.log(fullName);
+
+//To update the name in the picture card
+document.getElementById("trackedName").innerHTML = "Rosh 1";
+
+let email = document.getElementById("email").innerHTML;
+console.log(email);
+
+//To update the name in the picture card
+document.getElementById("trackedEmail").innerHTML = email;
+
+let phoneNumber = document.getElementById("phoneNumber").innerHTML;
+console.log(phoneNumber);
+
+let address = document.getElementById("address").innerHTML;
+console.log(address);
+
+//To update the name in the picture card
+document.getElementById("trackedAddress").innerHTML = address;
